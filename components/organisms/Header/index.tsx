8dbde3603bcf21346d8bbbcd2202ff490ee43ef8
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-07-11 17:21:29 +0200

    [Header] Improved [Images] Moved to folder public/images/

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -1,18 +1,21 @@
 import React, {FC} from "react";
 import HeaderProps from "./index.types"
 import Typography from "@atoms/Typography";
+import Image from "next/image";
+import ZC from "@images/ZC_Logo.svg";
 
 const Header: FC<HeaderProps> = ({}) => {
     return <>
-        <div className="flex flex-row justify-between">
+        <div className="flex flex-row justify-between pt-8">
             <div className="flex flex-row justify-between items-center">
-
+                <Image src={ZC} alt="ZC" width="50" height="50"/>
+                <Typography variant="label" color="neutral-1-color">EN</Typography>
             </div>
             <div className="flex flex-row justify-between items-center">
-                <Typography variant="label" color="neutral-1-color" className="pl-2.5">SKILLS</Typography>
-                <Typography variant="label" color="neutral-1-color" className="pl-2.5">WORKS</Typography>
-                <Typography variant="label" color="neutral-1-color" className="pl-2.5">ABOUT</Typography>
-                <Typography variant="label" color="neutral-1-color" className="pl-2.5">CONTACT</Typography>
+                <Typography variant="label" color="neutral-1-color" className="pl-8">SKILLS</Typography>
+                <Typography variant="label" color="neutral-1-color" className="pl-8">WORKS</Typography>
+                <Typography variant="label" color="neutral-1-color" className="pl-8">ABOUT</Typography>
+                <Typography variant="label" color="neutral-1-color" className="pl-8">CONTACT</Typography>
             </div>
         </div>
     </>
