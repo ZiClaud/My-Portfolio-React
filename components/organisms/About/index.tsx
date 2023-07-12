c7c4ebdcdd7358c39477502c9871886bf5ed1384
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-07-12 08:33:02 +0200

    [About] Work in progress

--- components/organisms/About/index.tsx
+++ components/organisms/About/index.tsx
@@ -1,9 +1,23 @@
 import React, {FC} from "react";
 import AboutProps from "./index.types"
+import Typography from "@atoms/Typography";
 
 const About: FC<AboutProps> = ({}) => {
     return <>
-        A bit about me
+        <Typography variant="h4" color="neutral-1" bold={true}>A BIT ABOUT ME</Typography>
+
+        <Typography variant="h3" color="neutral-2" bold={false}>
+            As a software developer, I am driven to create digital experiences that are both
+        </Typography>
+        <Typography variant="h3" color="neutral-1" bold={true}>
+            visually striking and intuitive to navigate.
+        </Typography>
+        <Typography variant="h3" color="neutral-2" bold={false}>
+            Besides programming, I am also deeply interested in
+        </Typography>
+        <Typography variant="h3" color="neutral-1" bold={true}>
+            design, music, and chess.
+        </Typography>
     </>
 }
 
