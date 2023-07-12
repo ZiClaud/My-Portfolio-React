7d8ac490214928b0b027955ab274d8a6b9f57760
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-07-12 08:19:37 +0200

    [Header] Minor improvements

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -9,8 +9,8 @@ const Header: FC<HeaderProps> = ({}) => {
     return <>
         <div className="flex flex-row justify-between pt-8 bg-black-background">
             <div className="flex flex-row justify-between items-center">
-                <Image src={ZC} alt="ZC" width="50" height="50"/>
-                <Typography variant="label" color="neutral-1">EN</Typography>
+                <Image src={ZC} alt="ZC" width="48" height="48"/>
+                <Typography variant="h5" color="neutral-1" bold={true}>EN</Typography>
             </div>
             <div className="flex flex-row justify-between items-center">
                 <HeaderButton link="#">SKILLS</HeaderButton>
