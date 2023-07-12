3b51628bfd5e20b894f56a55c5fc5534cbbd3e2e
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-07-12 08:46:38 +0200

    [Home] Cleaned

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Typography from "@atoms/Typography";
 import Hero from "@organisms/Hero";
 import Skills from "@organisms/Skills";
 import Works from "@organisms/Works";
@@ -15,14 +14,6 @@ export default function Home() {
             <About />
             <Contact />
             <Footer />
-            <Typography variant="h1" color="primary" bold={false}>h1: Hello World</Typography>
-            <Typography variant="h2" color="neutral-1" bold={false}>h2: Hello World</Typography>
-            <Typography variant="h3" color="neutral-2" bold={false}>h3: Hello World</Typography>
-            <Typography variant="h4" color="neutral-1" bold={false}>h4: Hello World</Typography>
-            <Typography variant="h5" color="neutral-1" bold={false}>h5: Hello World</Typography>
-            <Typography variant="body1" color="primary" bold={false}>body1: Hello World</Typography>
-            <Typography variant="button" color="neutral-1" bold={false}>button: Hello World</Typography>
-            <Typography variant="label" color="neutral-1" bold={false}>label: Hello World</Typography>
         </main>
     )
 }
