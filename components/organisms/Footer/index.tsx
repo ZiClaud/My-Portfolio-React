12e07dba1ed1d700c90e577f66810fbc2b01de51
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-07-12 08:35:29 +0200

    [Footer] Added copyright (Finished Footer)

--- components/organisms/Footer/index.tsx
+++ components/organisms/Footer/index.tsx
@@ -1,9 +1,14 @@
 import React, {FC} from "react";
 import FooterProps from "./index.types"
+import Typography from "@atoms/Typography";
 
 const Footer: FC<FooterProps> = ({}) => {
     return <>
-        © 2023 Claudio Di Maio
+        <div className="flex flex-row justify-center items-center">
+            <Typography variant="h5" color="neutral-2">
+                © 2023 Claudio Di Maio
+            </Typography>
+        </div>
     </>
 }
 
