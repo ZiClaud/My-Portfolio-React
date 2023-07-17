75896c7143fd674ab0ee15ebe08ceb140d2b79d2
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-07-17 20:14:31 +0200

    [Header] Testing React most important feature

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, {FC} from "react";
 import HeaderProps from "./index.types"
 import Image from "next/image";
@@ -5,7 +7,11 @@ import ZC from "@images/ZC_Logo.svg";
 import HeaderButton from "@molecules/HeaderButton";
 import HamburgerMenu from "@images/burger-menu.svg";
 
+const {useState} = React;
+
 const Header: FC<HeaderProps> = ({}) => {
+    const [counter, setCounter] = useState(0);
+
     return <>
         <div className="flex flex-row justify-between pt-8 bg-black-background">
             <div className="flex flex-row justify-between items-center">
@@ -21,6 +27,9 @@ const Header: FC<HeaderProps> = ({}) => {
                 <Image src={HamburgerMenu} alt={"-"} width={35} height={35}/>
             </div>
         </div>
+
+        <p>{counter}</p>
+        <button onClick={() => setCounter(counter + 1)}>Click me</button>
     </>
 }
 
