56940ac9096324c0b9b718ac380ec5af83d34967
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-07-17 20:21:03 +0200

    [Header] Menu now pops up when I click on the HamburgerMenu

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -10,7 +10,7 @@ import HamburgerMenu from "@images/burger-menu.svg";
 const {useState} = React;
 
 const Header: FC<HeaderProps> = ({}) => {
-    const [counter, setCounter] = useState(0);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return <>
         <div className="flex flex-row justify-between pt-8 bg-black-background">
@@ -23,13 +23,26 @@ const Header: FC<HeaderProps> = ({}) => {
                 <HeaderButton link="#">ABOUT</HeaderButton>
                 <HeaderButton link="#">CONTACT</HeaderButton>
             </div>
-            <div className="flex tablet:hidden">
-                <Image src={HamburgerMenu} alt={"-"} width={35} height={35}/>
-            </div>
+            <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                <div className="flex tablet:hidden">
+                    <Image src={HamburgerMenu} alt={"-"} width={35} height={35}/>
+                </div>
+            </button>
+            {isMenuOpen && (
+                <div className="tablet:hidden">
+                    <div className="bg-black-background fixed inset-0 flex items-center justify-center z-50">
+                        <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                            <div className="flex flex-col gap-4 text-white">
+                                <HeaderButton link="#">SKILLS</HeaderButton>
+                                <HeaderButton link="#">WORKS</HeaderButton>
+                                <HeaderButton link="#">ABOUT</HeaderButton>
+                                <HeaderButton link="#">CONTACT</HeaderButton>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
-
-        <p>{counter}</p>
-        <button onClick={() => setCounter(counter + 1)}>Click me</button>
     </>
 }
 
