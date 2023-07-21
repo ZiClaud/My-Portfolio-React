cfb020729bf33298c8adf434d948e3d67a5bb1b9
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-07-21 09:38:40 +0200

    [Works] Added "This Portfolio" work + added border to all work images

--- components/organisms/Works/index.tsx
+++ components/organisms/Works/index.tsx
@@ -7,18 +7,30 @@ const Works: FC<WorksProps> = ({}) => {
     return <>
         <div id="works-section">
             <SectionTitle text1="My best" text2="Works"/>
-            <WorksList imageFirst={false} title="Iremi App"
+            <WorksList imageFirst={true} title="Iremi App"
                        description="I developed this app entirely on my own, and it offers users a range of breathing exercises that are specifically designed to "
-                       descriptionBold="promote relaxation and mindfulness." category="Mobile App"
-                       urlImage="https://i.imgur.com/eoEx6Tt.png?1"
+                       descriptionBold="promote relaxation and mindfulness."
+                       category="Mobile App" urlImage="https://i.imgur.com/eoEx6Tt.png?1"
                        descriptionLong1={""}
-                       descriptionLongBold1={""} urlAllImages={[""]} skills={[""]}/>
-            <WorksList imageFirst={true} title="JEIOM Website"
+                       descriptionLongBold1={""}
+                       urlAllImages={[""]}
+                       skills={[""]}/>
+            <WorksList imageFirst={false} title="JEIOM Website"
                        description="I was part of a team that developed this website for the JEIOM 2023 event. Our goal was to create a platform that would enable users to "
                        descriptionBold="organize their schedule for the event in a single, user-friendly interface"
                        category="Website" urlImage="https://i.imgur.com/nu9WG4d.png"
-                       descriptionLong1={""} descriptionLongBold1={""} urlAllImages={[""]} skills={[""]}/>
-
+                       descriptionLong1={""}
+                       descriptionLongBold1={""}
+                       urlAllImages={[""]}
+                       skills={[""]}/>
+            <WorksList imageFirst={true} title="This Portfolio"
+                       description="I developed this portfolio, taking inspiration from a design found online, created first in  "
+                       descriptionBold="Flutter, then recreated in React."
+                       category="Website" urlImage="https://i.imgur.com/5yosKhp.png"
+                       descriptionLong1={""}
+                       descriptionLongBold1={""}
+                       urlAllImages={[""]}
+                       skills={[""]}/>
         </div>
     </>
 }
