ac7fd952bcff6a416f0fd5454459c7d60067cf61
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-07-31 21:25:27 +0200

    [AllWorksPage] Improved graphics

--- components/pages/AllWorksPage/index.tsx
+++ components/pages/AllWorksPage/index.tsx
@@ -1,27 +1,44 @@
-import React, {FC} from "react";
-import AllWorksPageProps from "./index.types"
+import React, { FC } from "react";
+import AllWorksPageProps from "./index.types";
 import Typography from "@atoms/Typography";
-import {AllWorks} from "@/database/fakeDB";
+import { AllWorks } from "@/database/fakeDB";
 import Image from "next/image";
 import Padding from "@atoms/Padding";
+import WorkData from "@/basics/WorkData";
 
 const AllWorksPage: FC<AllWorksPageProps> = ({}) => {
-    return <>
-        <div className="flex flex-col bg-black-background px-5 tablet:px-14 laptop:px-28">
-            <div className="flex flex-col justify-content-center items-center">
-                <Typography variant="h1" color="neutral-1" bold={true}>All of my Works</Typography>
-            </div>
-            <div>
-                {AllWorks.map((work) => {
-                    return <>
-                        <Typography variant="h3" color="neutral-1" bold={true}>{work.title}</Typography>
-                        <Image src={work.urlImage} alt={work.category} width={300} height={200}/>
-                        <Padding/>
-                    </>;
-                })}
-            </div>
-        </div>
-    </>
-}
+    const chunkWorks = (works: WorkData[], chunkSize: number) => {
+        const chunkedArray = [];
+        for (let i = 0; i < works.length; i += chunkSize) {
+            chunkedArray.push(works.slice(i, i + chunkSize));
+        }
+        return chunkedArray;
+    };
 
-export default AllWorksPage
+    return (
+        <>
+            <div className="flex flex-col bg-black-background px-5 tablet:px-14 laptop:px-28">
+                <div className="flex flex-col justify-between items-center">
+                    <Typography variant="h1" color="neutral-1" bold={true}>
+                        All of my Works
+                    </Typography>
+                    {chunkWorks(AllWorks, 3).map((chunk, index) => (
+                        <div key={index} className="flex justify-center items-center space-x-10">
+                            {chunk.map((work) => (
+                                <div key={work.title}>
+                                    <Typography variant="h3" color="neutral-1" bold={true}>
+                                        {work.title}
+                                    </Typography>
+                                    <Image src={work.urlImage} alt={work.category} width={300} height={200} />
+                                    <Padding />
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default AllWorksPage;
