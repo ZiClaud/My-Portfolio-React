f66a045dda1274cf699727d43b34f3d81eea80e1
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-08-05 12:34:49 +0200

    [Home] Finally fixed 404 problem

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import React, {useEffect, useState} from 'react';
 import HomePage from "@/components/pages/HomePage";
 import AllWorksPage from "../components/pages/AllWorksPage";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import LoadingPage from "@/components/pages/LoadingPage";
 import WorkPage from "@/components/pages/WorkPage";
 
@@ -23,12 +23,12 @@ export default function Home() {
 
     // When the document is loaded, render the routes
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
                 <Route path="/works" element={<AllWorksPage/>}/>
                 <Route path="/works/:linkName" element={<WorkPage/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
