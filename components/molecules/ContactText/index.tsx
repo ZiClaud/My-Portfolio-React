bd5d7c3f2542cf10a954cee936e8ad502862f59f
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-08-05 12:16:33 +0200

    [ContactText] Minor graphic & code improvements

--- components/molecules/ContactText/index.tsx
+++ components/molecules/ContactText/index.tsx
@@ -4,20 +4,13 @@ import Typography from "@atoms/Typography";
 
 const ContactText: FC<ContactTextProps> = ({}) => {
     return <>
-        <div className="flex flex-col justify-center items-center py-20 w-[350px]">
-            <div className="flex flex-row">
-                <Typography variant="h1" color="neutral-2">
-                    Get&nbsp;
-                </Typography>
-                <Typography variant="h1" color="neutral-1" bold={true}>
-                    in Touch
-                </Typography>
-            </div>
-            <div>
-                <Typography variant="body1" color="neutral-1">
-                    So that we can start working together!
-                </Typography>
-            </div>
+        <div className="flex flex-col justify-center items-center">
+            <Typography variant="h1" color="neutral-2">
+                Get <strong className="text-neutral-1-color">in Touch</strong>
+            </Typography>
+            <Typography variant="body1" color="neutral-1">
+                So that we can start working together!
+            </Typography>
         </div>
     </>
 }
