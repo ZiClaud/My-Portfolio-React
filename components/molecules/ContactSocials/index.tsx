d8b9c855d1cd021892654842b7cf97d3cfd7f51f
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-08-10 09:18:58 +0200

    [ContactSocials] Fixed hover bug

--- components/molecules/ContactSocials/index.tsx
+++ components/molecules/ContactSocials/index.tsx
@@ -5,18 +5,19 @@ import GitHubLogo from "@images/github_img.svg";
 import LinkedInLogo from "@images/linkedin_img.svg";
 import MailLogo from "@images/mail_img.svg";
 
-// TODO: Fix the absolute positioning of the icons -> Can't click because of div
 const ContactSocials: FC<ContactSocialsProps> = ({}) => {
     return <>
-        <div className="flex flex-col justify-center items-center h-[275px] w-[270px]">
-            <div className="absolute pr-[125px] pb-[125px]">
-                <IconButton src={GitHubLogo} alt="Github" href="https://github.com/ZiClaud"/>
-            </div>
-            <div className="absolute pl-[125px] pt-[0px]">
-                <IconButton src={LinkedInLogo} alt="LinkedIn" href="https://www.linkedin.com/in/claudio-di-maio"/>
-            </div>
-            <div className="absolute pr-[50px] pt-[200px]">
-                <IconButton src={MailLogo} alt="Mail" href="mailto:claudiodimaiozc@gmail.com"/>
+        <div className="h-[275px] w-[270px]">
+            <div className="absolute h-[275px] w-[270px]">
+                <div className="absolute left-[25px] top-[25px]">
+                    <IconButton src={GitHubLogo} alt="Github" href="https://github.com/ZiClaud"/>
+                </div>
+                <div className="absolute left-[145px] top-[80px]">
+                    <IconButton src={LinkedInLogo} alt="LinkedIn" href="https://www.linkedin.com/in/claudio-di-maio"/>
+                </div>
+                <div className="absolute left-[55px] top-[180px]">
+                    <IconButton src={MailLogo} alt="Mail" href="mailto:claudiodimaiozc@gmail.com"/>
+                </div>
             </div>
         </div>
     </>
