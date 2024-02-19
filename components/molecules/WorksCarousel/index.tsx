f32dff56cc10756c67dc1b7127d9b66b07d69275
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2024-02-19 16:54:04 +0100

    Revert "Database now 'supports' videos"
    This reverts commit 10dbda7a20f23f0732c61207e6dfea1e28874d6f.

--- components/molecules/WorksCarousel/index.tsx
+++ components/molecules/WorksCarousel/index.tsx
@@ -4,28 +4,10 @@ import WorksCarouselProps from "./index.types"
 
 import "./my-image-gallery.css"
 
-const isVideo = (url: string) => {
-  return url.endsWith(".mp4")
-}
-
-
 const WorksCarousel: FC<WorksCarouselProps> = ({ images }) => {
-  const items = images.map((imageUrl) => ({
-        original: imageUrl,
-        thumbnail: imageUrl,
-        renderItem: isVideo(imageUrl)
-          ? () => (
-            <video controls className="image-gallery-image">
-              <source src={imageUrl} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          )
-          : undefined, // Render undefined for images
-      }
-    ),
-  )
+  const items = images.map((imageUrl) => ({ original: imageUrl, thumbnail: imageUrl }))
 
-  return (
+  return <>
     <div className="w-[70vw] rounded-3xl overflow-auto">
       <ImageGallery
         items={items}
@@ -39,7 +21,7 @@ const WorksCarousel: FC<WorksCarouselProps> = ({ images }) => {
         showFullscreenButton={false}
       />
     </div>
-  )
+  </>
 }
 
 export default WorksCarousel
