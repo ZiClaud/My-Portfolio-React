7f5a0dd6fa176060893a7bbc91d8cd0df9162246
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2025-01-04 11:56:00 +0100

    Updated Images

--- components/molecules/AboutImages/index.tsx
+++ components/molecules/AboutImages/index.tsx
@@ -8,21 +8,21 @@ const AboutImages: FC<AboutImagesProps> = ({}) => {
     <div className="hidden laptop:block w-[40vw] h-[1000px]">
       <div className="absolute pt-[0]">
         <Image
-          src="https://i.imgur.com/IbPuHKd.jpeg"
+          src="https://i.imgur.com/YLpHJFu.jpeg"
           alt="404" width={282} height={374} className="rounded-3xl" object-fit="cover"
         />
       </div>
 
       <div className="absolute pt-[300px] pl-[300px]">
         <Image
-          src="https://i.imgur.com/OHOXqdj.jpg"
+          src="https://i.imgur.com/DG9AuJZ.jpeg"
           alt="404" width={282} height={374} className="rounded-3xl" object-fit="cover"
         />
       </div>
 
       <div className="absolute pt-[600px]">
         <Image
-          src="https://i.imgur.com/30Vnhka.jpeg"
+          src="https://i.imgur.com/dQtPUZB.jpeg"
           alt="404" width={282} height={374} className="rounded-3xl" object-fit="cover"
         />
       </div>
