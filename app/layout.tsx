0c5633b7ae01c37894073ff79e9a1f08a47dc5de
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2026-02-19 11:26:33 +0100

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import React from "react"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Claudio Di Maio's Portfolio",
-  description: "",
+  title: "Claudio Di Maio | Software Developer & Open-Source Contributor",
+  description: "Developer with expertise in TypeScript, React, and Flutter. Passionate about creating scalable, user-friendly web applications and contributing to open-source projects",
 }
 
 export default function RootLayout({ children }: {
